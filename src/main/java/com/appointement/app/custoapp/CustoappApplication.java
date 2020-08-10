de14102925ee1ae0c5b464e632c3f1ccd52ee9ff
smlc <siesa.marlon@hotmail.fr>
2020-08-10 22:41:05 +0100

    init !

---
+++ src/main/java/com/appointement/app/custoapp/CustoappApplication.java
@@ -0,0 +1,13 @@
+package com.appointement.app.custoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustoappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustoappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/appointement/app/custoapp/CustoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointement.app.custoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustoappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
