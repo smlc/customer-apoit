cbce47767aff4ae309cf9d989b28e9d8265fd5fa
smlc <siesa.marlon@hotmail.fr>
2020-09-19 12:58:26 +0100

    fix error in rest api path and alpine js cdn link typo

--- src/main/java/com/appointement/app/custoapp/controller/RestApiController.java
+++ src/main/java/com/appointement/app/custoapp/controller/RestApiController.java
@@ -2,22 +2,28 @@ package com.appointement.app.custoapp.controller;
 
 import java.util.List;
 
-import com.appointement.app.custoapp.beans.AppointmentInfo;
 import com.appointement.app.custoapp.beans.DashboardData;
 import com.appointement.app.custoapp.services.db.AppointmentDB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+	private final Logger log = LoggerFactory.getLogger(RestApiController.class);
 
 	@Autowired
 	AppointmentDB appointmentDB;
 
 	@GetMapping("/appointment")
 	public List<DashboardData> appointment() {
+		log.info("Dashboard get all appointment");
 		return appointmentDB.findAll();
 	}
 }
